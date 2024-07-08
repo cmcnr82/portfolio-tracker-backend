b9d48131b8279a54b43b899978f6768b81f9c7b6
cmcnr82 <cmcnr82@gmail.com>
2024-07-08 22:09:33 +0300

    test microservice added.

---
+++ services/test/src/main/java/com/portfolio_tracker/test/controller/Controller.java
@@ -0,0 +1,19 @@
+package com.portfolio_tracker.test.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/public")
+    public ResponseEntity<String> get() {
+        return ResponseEntity.ok("Public...");
+    }
+
+    @GetMapping("/private")
+    public ResponseEntity<String> bye() {
+        return ResponseEntity.ok("Private...");
+    }
+}
