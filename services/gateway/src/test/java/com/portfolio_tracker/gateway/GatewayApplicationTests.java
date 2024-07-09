32a391b661739ea038de49e5d39c92d341980e78
cmcnr82 <cmcnr82@gmail.com>
2024-07-09 21:16:48 +0300

    gateway service added.

---
+++ services/gateway/src/test/java/com/portfolio_tracker/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio_tracker.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
