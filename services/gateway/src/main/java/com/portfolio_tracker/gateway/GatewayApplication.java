074a6342aae35d175cf21de38e76f0b9df29927a
cmcnr82 <cmcnr82@gmail.com>
2024-07-14 22:13:07 +0300

    reactive gateway changed.

--- services/gateway/src/main/java/com/portfolio_tracker/gateway/GatewayApplication.java
+++ services/gateway/src/main/java/com/portfolio_tracker/gateway/GatewayApplication.java
@@ -2,13 +2,18 @@ package com.portfolio_tracker.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
 
 	public static void main(String[] args) {
+		// Retrieve execution profile from environment variable. Otherwise, default profile is used.
+		String profile = System.getenv("PROFILE");
+		System.setProperty("spring.profiles.active", profile != null ? profile : "default");
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 	// KeycloakLogoutHandler needs a bean of type RestTemplate
