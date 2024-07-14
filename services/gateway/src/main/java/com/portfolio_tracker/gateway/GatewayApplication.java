7f3f8ab0be60b4a7eca6c68a76ec6a3dcb248431
cmcnr82 <cmcnr82@gmail.com>
2024-07-14 12:35:46 +0300

    rest template added.

--- services/gateway/src/main/java/com/portfolio_tracker/gateway/GatewayApplication.java
+++ services/gateway/src/main/java/com/portfolio_tracker/gateway/GatewayApplication.java
@@ -2,6 +2,8 @@ package com.portfolio_tracker.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -9,5 +11,9 @@ public class GatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
-
+	// KeycloakLogoutHandler needs a bean of type RestTemplate
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
